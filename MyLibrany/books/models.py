5103f4ff7468dfe813753b9c3db6ceea2dcfdfe0
MilanSad <milansadilek@post.cz>
2024-04-03 18:53:05 +0200

    Category models

--- MyLibrany/books/models.py
+++ MyLibrany/books/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Autor(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=500)
+
+class Book(models.Model):
+    name = models.CharField(max_lenght=100)
+    description  = models.CharField(max_lenght=500)
+    on_stock = models.BooleanField(default=True)
+    category = models.ManyToManyField(Category)
+    autor = models.ManyToManyField(Autor)
+
+
+
+
